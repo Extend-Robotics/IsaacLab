c2d3f23a4ec64c05c27d34dd852dab36936f72cb
5hirgur-extend <pranav.shirgur@extendrobotics.com>
2025-10-16 13:34:53 +0100

    only image name changed to reflect final image, this needs to be studied and streamlined

--- docker/utils/container_interface.py
+++ docker/utils/container_interface.py
@@ -68,7 +68,7 @@ class ContainerInterface:
             # insert a hyphen before the suffix if a suffix is given
             self.suffix = f"-{suffix}"
 
-        self.container_name = "er-ros1-isaac-lab-base"
+        self.container_name = "er-ros1-isaac5"
         self.image_name = f"isaac-lab-{self.profile}{self.suffix}:latest"
 
         # keep the environment variables from the current environment,
@@ -131,7 +131,7 @@ class ContainerInterface:
                     "--env-file",
                     ".env.base",
                     "build",
-                    "er-ros1-isaac-lab-base",
+                    "er-ros1-isaac5",
                 ],
                 check=False,
                 cwd=self.context_dir,
